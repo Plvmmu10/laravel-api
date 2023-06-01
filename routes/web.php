44d1f435049db816a43ae2dcb52e9099756673a0
peppeluc22 <gluca0260@gmail.com>
2023-06-01 14:10:13 +0200

    init

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
+    // Route to the dashboard
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+});
+
+
+require __DIR__ . '/auth.php';
