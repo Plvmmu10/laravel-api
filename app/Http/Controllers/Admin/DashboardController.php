62f723aad008153ad180aa960307ffba0a34bab0
peppeluc22 <gluca0260@gmail.com>
2023-06-01 15:29:15 +0200

    removed user in controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $users = User::all();
         return view('admin.dashboard', compact('users'));
     }
 }
