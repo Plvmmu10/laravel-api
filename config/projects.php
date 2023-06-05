dc9842df7bd18f3a91263b34e52191733971b7c3
peppeluc22 <gluca0260@gmail.com>
2023-06-05 16:25:48 +0200

    created projects array

---
+++ config/projects.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'projects' => [
+        [
+            'name' => "Laravel Auth",
+            'image' => "https://www.google.com/imgres?imgurl=https%3A%2F%2Flaravelarticle.com%2Ffilemanager%2Fuploads%2Flaravel-8-auth.png&tbnid=ACstmKjRwVFFMM&vet=12ahUKEwjm6MvAkqz_AhUR3KQKHVZFADkQMygJegUIARDOAQ..i&imgrefurl=https%3A%2F%2Flaravelarticle.com%2Flaravel-8-authentication-tutorial&docid=H2TzQJ5JFgtlZM&w=640&h=360&q=laravel%20auth&ved=2ahUKEwjm6MvAkqz_AhUR3KQKHVZFADkQMygJegUIARDOAQ",
+            'description' => "This exercise is about using laravel authentication functionality"
+        ],
+        [
+            'name' => "Laravel DC Comics",
+            'image' => "https://www.google.com/imgres?imgurl=https%3A%2F%2Flaravelarticle.com%2Ffilemanager%2Fuploads%2Flaravel-8-auth.png&tbnid=ACstmKjRwVFFMM&vet=12ahUKEwjm6MvAkqz_AhUR3KQKHVZFADkQMygJegUIARDOAQ..i&imgrefurl=https%3A%2F%2Flaravelarticle.com%2Flaravel-8-authentication-tutorial&docid=H2TzQJ5JFgtlZM&w=640&h=360&q=laravel%20auth&ved=2ahUKEwjm6MvAkqz_AhUR3KQKHVZFADkQMygJegUIARDOAQ",
+            'description' => "This exercise is about using laravel to make DC Comics Site"
+        ],
+        [
+            'name' => "Laravel University",
+            'image' => "https://www.google.com/imgres?imgurl=https%3A%2F%2Flaravelarticle.com%2Ffilemanager%2Fuploads%2Flaravel-8-auth.png&tbnid=ACstmKjRwVFFMM&vet=12ahUKEwjm6MvAkqz_AhUR3KQKHVZFADkQMygJegUIARDOAQ..i&imgrefurl=https%3A%2F%2Flaravelarticle.com%2Flaravel-8-authentication-tutorial&docid=H2TzQJ5JFgtlZM&w=640&h=360&q=laravel%20auth&ved=2ahUKEwjm6MvAkqz_AhUR3KQKHVZFADkQMygJegUIARDOAQ",
+            'description' => "This exercise is about creating an University Database"
+        ],
+    ]
+];
