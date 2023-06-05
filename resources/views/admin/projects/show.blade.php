66d05b0e128c118789b4f42fdadd7926e27cca5c
peppeluc22 <gluca0260@gmail.com>
2023-06-05 16:27:46 +0200

    created each project show

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="col-12 showCol">
+            <div class="card">
+                <div class="card-image">
+                    <img src="{{ $project->image }}" alt="{{ $project->name }}">
+                </div>
+
+                <div class="card-body">
+                    <h2>{{ $project->name }}</h2>
+                    <div class="description p-2">
+                        <p>{{ $project->description }}</p>
+                    </div>
+
+                    <div class="pt-5">
+                        <button class="btn btn-outline-success">
+                            <a href="{{ route('admin.projects.index') }}">Project List</a>
+                        </button>
+
+                        <button class="btn btn-outline-primary">
+                            <a href="{{ route('admin.projects.edit', $project->slug) }}">Edit</a>
+                        </button>
+
+                        <button class="btn btn-outline-danger">
+                            <a href="{{ route('admin.projects.destroy', $project->slug) }}">Delete</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
