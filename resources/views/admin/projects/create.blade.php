9f5c7f982ea66eedcc8004ccfff2844c90a6e012
peppeluc22 <gluca0260@gmail.com>
2023-06-06 14:21:42 +0200

    added errors

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -3,28 +3,42 @@
 @section('content')
     <div class="container d-flex justify-content-center">
         <div class="w-50 text-white mt-5">
-            <form action="" method="POST">
+            <form action="{{ route('admin.projects.store') }}" method="POST">
                 @csrf
-                @method('PUT')
                 {{-- Name --}}
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="name" name="name" ">
-                                <div id="nameText" class="form-text text-white">Choose your project name</div>
-                            </div>
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="name"
+                        name="name">
+                    <div id="nameText" class="form-text text-white">Choose your project name</div>
 
-                            {{-- Image --}}
-                            <div class="mb-3">
-                                <label for="image" class="form-label">Image</label>
-                                <input type="text" class="form-control" id="image" name="image" ">
-                    <div id="imageText" class="form-text text-white">Choose your project image</div>
+                    @error('title')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
 
-                {{-- Name --}}
+                {{-- Image --}}
+                <div class="mb-3">
+                    <label for="image" class="form-label">Image</label>
+                    <input type="text" class="form-control @error('image') is-invalid @enderror" id="image"
+                        name="image">
+                    <div id="imageText" class="form-text text-white">Choose your project image</div>
+
+                    @error('image')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                {{-- Description --}}
                 <div class="mb-3">
                     <label for="description" class="form-label">Description</label>
-                    <textarea type="text" class="form-control" id="description" name="description"></textarea>
+                    <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description"
+                        name="description"></textarea>
                     <div id="descriptionText" class="form-text text-white">Choose your project description</div>
+
+                    @error('description')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <button type="submit" class="btn btn-outline-primary">Create</button>
