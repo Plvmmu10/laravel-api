84ff76c46ab20f4b148d873111389f036201d831
peppeluc22 <gluca0260@gmail.com>
2023-06-06 14:21:19 +0200

    changed some columns config

--- database/migrations/2023_06_05_122630_create_projects_table.php
+++ database/migrations/2023_06_05_122630_create_projects_table.php
@@ -16,9 +16,9 @@ return new class extends Migration {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug');
-            $table->text('image');
-            $table->text('description');
+            $table->string('slug')->unique();
+            $table->text('image')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
