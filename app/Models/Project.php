0702ffa82a07edc583b5654c21d020dc3ee6aef6
peppeluc22 <gluca0260@gmail.com>
2023-06-07 15:54:31 +0200

    created relations from project to technology

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug', 'image', 'description'];
+    protected $fillable = ['name', 'slug', 'image', 'description', 'technologies_id'];
+
+    public function technology(): BelongsTo
+    {
+        return $this->belongsTo(Technology::class);
+    }
 }
