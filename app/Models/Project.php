8ba716edfb20468e5d71a65807b42247cf46c230
peppeluc22 <gluca0260@gmail.com>
2023-06-08 17:01:14 +0200

    created relations to tag

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
@@ -15,5 +16,10 @@ class Project extends Model
     public function technology(): BelongsTo
     {
         return $this->belongsTo(Technology::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
